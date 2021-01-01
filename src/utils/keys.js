af0a30101b25adb0737939d70e7c0a39f2440d69
ajaymovva <majaykumar51@gmail.com>
2021-01-01 22:35:13 +0530

    Removed mongouri

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,6 +1,5 @@
 const keyValues = {
     JWT_TOKEN : 'asljljlsdkfj',
-    MONGOURI: "mongodb+srv://ajay:8KHt82mppGwGM5Qa@cluster0.ydh58.mongodb.net/Instagram?retryWrites=true&w=majority"
 }
 module.exports = keyValues;
 
